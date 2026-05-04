ab4426b3a79542b08789e6029d81bc0d25c3af14
Swara <swarnalata.panigrahy20@gmail.com>
2026-05-04 10:48:16 -0700

    Update SR-1L-POL_symbolic_head.h

--- NeuSym-main/hls_generated/SR-1L-POL_symbolic_head.h
+++ NeuSym-main/hls_generated/SR-1L-POL_symbolic_head.h
@@ -1,11 +1,9 @@
-// SR-1L-POL_symbolic_head.h
-// Auto-generated HLS header for experiment: SR-1L-POL
-
 #ifndef SR_1L_POL_SYMBOLIC_HEAD_H
 #define SR_1L_POL_SYMBOLIC_HEAD_H
 
 #define SYMBOLIC_DIM 128
+#define SYMBOLIC_NUM_TERMS 8
 
 float symbolic_head(float x[SYMBOLIC_DIM]);
 
-#endif  // SR_1L_POL_SYMBOLIC_HEAD_H
+#endif
