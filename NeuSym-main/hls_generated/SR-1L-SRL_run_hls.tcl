89c586e82cf93f09c2ea8215ab14cc3c82995691
Swara <swarnalata.panigrahy20@gmail.com>
2026-05-12 10:17:59 -0700

    Update SR-1L-SRL_run_hls.tcl

--- NeuSym-main/hls_generated/SR-1L-SRL_run_hls.tcl
+++ NeuSym-main/hls_generated/SR-1L-SRL_run_hls.tcl
@@ -1,18 +1,14 @@
-# hls/run_hls.tcl — SR-1L-SCE
-
+# hls/run_hls.tcl — SR-1L-SRL
 open_project symbolic_head_proj
 set_top symbolic_head
-add_files {../hls_generated/SR-1L-SCE_symbolic_head.cpp}
-
+add_files {../hls_generated/SR-1L-SRL_symbolic_head.cpp}
+add_files {../hls_generated/SR-1L-SRL_symbolic_head.h}
 open_solution "solution1"
 set_part {xc7z020clg484-1}
 create_clock -period 10 -name default
-
 csynth_design
 export_design -format ip_catalog
-
 file copy -force \
     symbolic_head_proj/solution1/syn/report/symbolic_head_csynth.rpt \
-    SR_1L_SCE_REPORT.rpt
-
+    SR_1L_SRL_REPORT.rpt
 close_project
